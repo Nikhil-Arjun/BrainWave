593a41d6608a3f15be0b0a19fc053717078ed420
Nikhil-Arjun <nikhilarjun97@gmail.com>
2024-03-12 18:07:52 +0530

    Updating main root directory

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -15,6 +18,9 @@ export default function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
